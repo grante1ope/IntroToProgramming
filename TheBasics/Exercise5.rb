8ff5f3133061859e731cf06db2779ee5a812f267
Grant Cherwenuk <gcherwenuk@gmail.com>
2018-12-12 14:02:22 -0800

    Updating with completed exercises

--- TheBasics/Exercise5.rb
+++ TheBasics/Exercise5.rb
@@ -1,2 +1,16 @@
 #Intro to Programming: The Basics - Exercise# 5
 #By Grant Cherwenuk
+def factorial(n)
+  factor = n
+  c = n -1
+  until c == 1
+    factor *= c
+    c -= 1
+  end
+  puts "#{factor}"
+end
+
+factorial(5)
+factorial(6)
+factorial(7)
+factorial(8)
